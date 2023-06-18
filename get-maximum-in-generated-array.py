4bd1007cc220e9a9a701ecb1beb7dbe18210b39a
Yakin <59527205+yakin-ts@users.noreply.github.com>
2023-06-18 23:27:09 +0300

    added get-maximum-in-generated-array.python

---
+++ get-maximum-in-generated-array.py
@@ -0,0 +1,21 @@
+class Solution:
+    def getMaximumGenerated(self, n: int) -> int:
+        if n==0:
+            return 0
+        elif n==1:
+            return 1
+
+        dp = [1 for _ in range(n+1)]
+        dp[0] = 0 
+    
+        def helper(idx):
+            if idx > ceil(n//2):
+                return
+            if 2*idx <= n:
+                dp[2*idx] = dp[idx]
+            if 2*idx + 1 <= n:
+                dp[2*idx+1] = dp[idx] + dp[idx+1]
+            return helper(idx+1)
+
+        helper(0)
+        return max(dp)
