6d729a85be6a09bb87fcdf4ab0425a64926a90a7
Yakin <59527205+yakin-ts@users.noreply.github.com>
2023-10-19 01:21:45 +0300

    added lexicographically-smallest-equivalent-string.python

--- lexicographically-smallest-equivalent-string.py
+++ lexicographically-smallest-equivalent-string.py
@@ -1,38 +1,36 @@
+class UnionFind:
+    def __init__(self,stx1,stx2):
+        self.root={}
+        for ch in stx1:
+            self.root[ch] = ch
+        for ch in stx2:
+            self.root[ch] = ch
+    def find(self,stx):
+        if stx==self.root[stx]:
+            return self.root[stx]
+        self.root[stx] = self.find(self.root[stx])
+        return self.root[stx]
+    
+    def union(self,ch1,ch2):
+        x = self.find(ch1)
+        y = self.find(ch2)
+        if x!=y:
+            if x > y:
+                self.root[x] = y
+            else:
+                self.root[y] = x
+        
 class Solution:
     def smallestEquivalentString(self, s1: str, s2: str, baseStr: str) -> str:
+        N = len(s1)
+        uf = UnionFind(s1,s2)
+        for i in range(N):
+            uf.union(s1[i],s2[i])
         
-
-        root = defaultdict(str)
-        def initialize_ds(stx1,stx2):
-            for c in stx1:
-                root[c] = c
-            for c in stx2:
-                root[c] = c
-        initialize_ds(s1,s2)
-
-        def find(x):
-            if x == root[x]:
-                return root[x]
-            root[x] = find(root[x])
-            return root[x]
-            
-        def union(x,y):
-            node1 = find(x)
-            node2 = find(y)
-            if node1 != node2:
-                if node1 < node2:
-                    root[node2] = node1
-                else:
-                    root[node1] = node2
-                    
         answer = ''
-
-        for i in range(len(s1)):
-            union(s1[i],s2[i])
-        for c in baseStr:
-            if c in root:
-                answer += find(c)
+        for ch in baseStr:
+            if ch in uf.root:
+                answer += uf.find(ch)
             else:
-                answer += c
-        print(root)
+                answer += ch
         return answer
